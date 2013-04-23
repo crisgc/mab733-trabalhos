0f3b7ddf346da2311000a96e556a19fccfc6bbbc
crisgc1@gmail.com <crisgc1@gmail.com@540b4fbd-5547-afed-6b2a-10894cb8bde3>
2013-04-23 19:05:01 +0000

    Fazendo o WSDL do serviço funcionar

--- factorial/fatorial.php
+++ factorial/fatorial.php
@@ -1,50 +1,37 @@
 <?php
-
-
 require_once("../nusoap/lib/nusoap.php");
-
-echo 'Fatorial 5: '.fatorial(5);
  
 $server = new nusoap_server;
  
-$server->configureWSDL('server');
+$server->configureWSDL('fatorial');
  
 //$server->wsdl->schemaTargetNamespace = 'urn:server';
  
 //SOAP complex type return type (an array/struct)
-// $server->wsdl->addComplexType(
-    // 'Person',
-    // 'complexType',
-    // 'struct',
-    // 'all',
-    // '',
-    // array(
-        // 'id_user' => array('name' => 'id_user', 'type' => 'xsd:int'),
-        // 'fullname' => array('name' => 'fullname', 'type' => 'xsd:string'),
-        // 'email' => array('name' => 'email', 'type' => 'xsd:string'),
-        // 'level' => array('name' => 'level', 'type' => 'xsd:int')
-    // )
-// );
+$server->wsdl->addComplexType(
+    'Person',
+    'complexType',
+    'struct',
+    'all',
+    '',
+    array(
+        'id_user' => array('name' => 'id_user', 'type' => 'xsd:int'),
+        'fullname' => array('name' => 'fullname', 'type' => 'xsd:string'),
+        'email' => array('name' => 'email', 'type' => 'xsd:string'),
+        'level' => array('name' => 'level', 'type' => 'xsd:int')
+    )
+);
  
 //first simple function
-$server->register('hello',
-			array('username' => 'xsd:string'),  //parameter
-			array('return' => 'xsd:string'),  //output
-			false,   //namespace
-			false,  //soapaction
-			'rpc', // style
-			'encoded', // use
-			'Just say hello');  //description
-			
-// $server->register('fatorial',
-			// array('number' => 'xsd:int'),  //parameter
-			// array('return' => 'xsd:int'),  //output
+// $server->register('hello',
+			// array('username' => 'xsd:string'),  //parameter
+			// array('return' => 'xsd:string'),  //output
 			// false,   //namespace
 			// false,  //soapaction
 			// 'rpc', // style
 			// 'encoded', // use
-			// 'Calcula o fatorial de um número');  //description
- 
+			// 'Just say hello');  //description
+			
 //this is the second webservice entry point/function 
 // $server->register('login',
 			// array('username' => 'xsd:string', 'password'=>'xsd:string'),  //parameters
@@ -60,18 +47,15 @@ function hello($username) {
         return 'Howdy, '.$username.'!';
 }
 
-function fatorial($number) {
- 
-// Caso seja fatorial de 0
-$result = 1;
-// Calculando o fatorial
-for($i = $number; $i > 1; $i--) {
-	$result = $result * $i;
-}
+$server->register('fatorial',
+			array('number' => 'xsd:int'),  //parameter
+			array('return' => 'xsd:int'),  //output
+			false,   //namespace
+			false,  //soapaction
+			'rpc', // style
+			'encoded', // use
+			'Calcula o fatorial de um número');  //description
 
-return $result;
-}
- 
 //second function implementation 
 function login($username, $password) {
         //should do some database query here
@@ -84,7 +68,17 @@ function login($username, $password) {
 		'level'=>99
 	);
 }
- 
+
+function fatorial($number) { 
+	// Caso seja fatorial de 0
+	$result = 1;
+	// Calculando o fatorial
+	for($i = $number; $i > 1; $i--) {
+		$result = $result * $i;
+	}
+	return $result;
+}
+
 $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
  
 $server->service($HTTP_RAW_POST_DATA);
